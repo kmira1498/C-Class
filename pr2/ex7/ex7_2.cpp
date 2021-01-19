cb60620ca98eda85ad96b01cb9bfe370c3622d0c
Konstantinos <kmira1498@gmail.com>
2021-01-19 00:16:14 +0000

    finished pr2

--- pr2/ex7/ex7_2.cpp
+++ pr2/ex7/ex7_2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // recursive method 1 function
 int counter1 = 0;
-float rm1(float a,unsigned int n)
+float rm1(float a, int n)
 {
   counter1++;
 
@@ -17,7 +17,7 @@ float rm1(float a,unsigned int n)
 
 // recursive method 2 function
 int counter2 = 0;
-float rm2(float a, unsigned int n)
+float rm2(float a, int n)
 {
   counter2++;
 
@@ -35,20 +35,21 @@ float rm2(float a, unsigned int n)
   }
 }
 
-int main(){
+int main()
+{
   int a;
-    unsigned int n;
+  int n;
 
-    cout << "Enter a number as the base: ";
-    cin >> a;
+  cout << "Enter a number as the base: ";
+  cin >> a;
 
-    cout << "Enter a number as the power: ";
-    cin >> n;
+  cout << "Enter a number as the power: ";
+  cin >> n;
 
-    cout << "The First Recursion Method provides the following answer : " << "(" << a << ")^(" << n << ") = " << rm1(a,n) << "; The number of recursive calls were:  " << counter1 << endl;
+  cout << "The First Recursion Method provides the following answer : " << "(" << a << ")^(" << n << ") = " << rm1(a,n) << "; The number of recursive calls were:  " << counter1 << endl;
 
-    cout << "The Second Recursion Method provides the following answer : " << "(" << a << ")^(" << n << ") = " << rm2(a,n) << "; The number of recursive calls were:  " << counter2 << endl;
+  cout << "The Second Recursion Method provides the following answer : " << "(" << a << ")^(" << n << ") = " << rm2(a,n) << "; The number of recursive calls were:  " << counter2 << endl;
 
-    return 0;
+   return 0;
 }
     //For the First Recursion Method, whether the power is odd or even the number of function calls is always n + 1; As for the Second Recursion Method, when the power is odd the number of function calls is n, but when the power is even the number of function calls is always n + 1;
